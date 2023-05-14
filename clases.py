e62255641ee9a5dda1368fa32c0a10c39b4d8aa9
MarianoSoto412 <mariano@darknexus.city>
2023-05-14 17:59:12 -0600

    init

---
+++ clases.py
@@ -0,0 +1,9 @@
+
+class Paciente:
+    def __init__(self):
+        cedula=""
+        nombreCompleto=("nombre", "apellido1", "apellido2")
+        correo = ""
+        anotaciones=[]
+        activo = True
+    
---
+++ pacientes.py
@@ -0,0 +1,64 @@
+import archivos
+
+
+
+def ESReportes(pDicc):
+    """
+    Funcionalidad: Muestra menu de reportes
+    Entradas:
+    -pDicc(dict): El diccionario a analizar
+    Salidas:
+    -pDicc(dict): El diccionario analizado
+    """
+    menuDicc = {
+
+        1: ["Por personalidad", ESReportePersonalidades],
+        2: ["Por categoría", ESPorCategorias],
+        3: ["Por persona", ESReportePersona],
+        4: ["Reporte total", ESReporteTotal],
+        5: ["Salir a menu", SalirReporte]
+    }
+    while True:
+        for key in menuDicc:
+            print(f"{key}. {menuDicc[key][0]}")
+        try:
+            opcion = int(input("Ingrese el número de su opción a elegir: "))
+            menuDicc[opcion][1](pDicc)
+            if opcion == 5:
+                break
+        except ValueError:
+            print("Por favor ingrese un número válido")
+        except KeyError:
+            print("Opción inválida")
+    return pDicc
+
+def menu():
+    """
+    Funcionalidad: Muestra menu principal
+    Entradas:NA
+    Salidas:NA
+    """
+    personalidad = archivos.lee("personalidad") or {}
+    ESRegistrarDatos(personalidad)
+    while validarBin(input("Desea registrar más personas? (1: sí, 2: no): ")):
+        ESRegistrarDatos(personalidad)
+    archivos.graba("personalidad", personalidad)
+    menuDicc = {
+        1: ["Registrar Datos", ESRegistrarDatos],
+        2: ["Modificar Datos", ESModificarDatos],
+        3: ["Eliminar Datos", ESEliminarDatos],
+        4: ["Reportes", ESReportes],
+        5: ["Salir", exit]
+    }
+    
+    while True:
+        for key in menuDicc:
+            print(f"{key}. {menuDicc[key][0]}")
+        try:
+            opcion = int(input("Ingrese el número de su opción a elegir: "))
+            personalidad = menuDicc[opcion][1](personalidad)
+            archivos.graba("personalidad", personalidad)
+        except ValueError:
+            print("Por favor ingrese un número válido")
+        except KeyError:
+            print("Opción inválida")
